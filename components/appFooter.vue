2eb634259851bafa50e13863840ca6cbf8bcac02
Kemeow815 <kemiao@kmblog.icu>
2025-05-23 22:53:05 +0800

    更新配置和内容，优化页面展示

--- components/appFooter.vue
+++ components/appFooter.vue
@@ -1,4 +1,4 @@
 <template>
-	<div class="text-sm text-gray-600 dark:text-gray-400">Copyright ©2025.sooooooooooooooooootheby</div>
+	<div class="text-sm text-gray-600 dark:text-gray-400">Copyright ©2025.克喵爱吃卤面</div>
 	<theme />
 </template>
